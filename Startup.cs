c9c10919b8d08beb75b544e970355d0467bc71c4
NguyenThuanThien <thiennhi063@gmail.com>
2021-06-28 11:02:51 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NguyenThuanThien_BigSchool.Startup))]
+namespace NguyenThuanThien_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
