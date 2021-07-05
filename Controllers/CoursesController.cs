51fafee227172901404749f7d59bbba06a486b6e
NguyenThuanThien <thiennhi063@gmail.com>
2021-07-05 09:06:41 +0700

    tao view create

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NguyenThuanThien_BigSchool.Models;
+using NguyenThuanThien_BigSchool.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,21 @@ namespace NguyenThuanThien_BigSchool.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbConText;
+
+        public CoursesController()
+        {
+            _dbConText = new ApplicationDbContext();
+        }
+
         // GET: Courses
         public ActionResult Create()
         {
-            return View();
+            var ViewModel = new CourseViewModel
+            {
+                Categories = _dbConText.Categories.ToList()
+            };
+            return View(ViewModel);
         }
     }
 }
